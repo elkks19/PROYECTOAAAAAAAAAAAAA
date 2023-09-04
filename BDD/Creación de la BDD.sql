8e0970fba268de832a813feed88a0bad59fbc1a7
Rafael <rafafabiani1909@gmail.com>
2023-09-04 17:19:05 -0400

    Primera versión del perfil terminada y algunos añadidos en el script de creación de la base de datos

--- BDD/Creación de la BDD.sql
+++ BDD/Creación de la BDD.sql
@@ -1,37 +1,106 @@
+
+USE PruebasProyecto;
+
+--Tablas independientes
+
 CREATE TABLE Empresas(
 	id INT IDENTITY(1, 1),
-	CodEMP VARCHAR(45) NOT NULL PRIMARY KEY,
-	NombreEMP VARCHAR(150) NOT NULL,
+	codEmpresa VARCHAR(45) NOT NULL PRIMARY KEY,
+	nombreEmpresa VARCHAR(150) NOT NULL,
+	direccionEmpresa VARCHAR(50) NOT NULL,
+
 	CreatedAt TIMESTAMP NOT NULL,
 	LastUpdate TIMESTAMP NOT NULL,
-	
 );
 
 CREATE TABLE Personas(
 	id INT IDENTITY(1, 1),
-	CodPER VARCHAR(45) NOT NULL PRIMARY KEY,
-	NombrePER VARCHAR(70) NOT NULL,
-	ApPaternoPER VARCHAR(70) NOT NULL,
-	ApMaternoPER VARCHAR(70) NOT NULL,
-	FechaNacPER VARCHAR(70) NOT NULL,
-	MailPER VARCHAR(50) NOT NULL,
-	CiPER VARCHAR(10) NOT NULL,
+	codPersona VARCHAR(45) NOT NULL PRIMARY KEY,
+	nombrePersona VARCHAR(70) NOT NULL,
+	apPaternoPersona VARCHAR(70) NOT NULL,
+	apMaternoPersona VARCHAR(70) NOT NULL,
+	fechaNacPersona VARCHAR(70) NOT NULL,
+	mailPersona VARCHAR(50) NOT NULL,
+	ciPersona VARCHAR(10) NOT NULL,
+	direccionPersona VARCHAR(50) NOT NULL,
+	userPersona VARCHAR(20) NOT NULL,
+	passwordPersona VARCHAR(20) NOT NULL,
 
 	CreatedAt TIMESTAMP NOT NULL,
 	LastUpdate TIMESTAMP NOT NULL,
 );
 
+--Tablas dependientes pero que son para cosas que se sienten como independientes
 CREATE TABLE Usuarios(
-	id INT IDENTITY(1, 1),
-	CodUSU VARCHAR(45) NOT NULL PRIMARY KEY,
+	codPersona INT NOT NULL,
+	codUsuario INT NOT NULL,
+
 );
 
 CREATE TABLE Personal_Empresas(
-	id INT IDENTITY(1, 1),
-	CodPEMP VARCHAR(45) NOT NULL PRIMARY KEY,
-	CodPER FOREIGN KEY
-	CONSTRAINT FK_PersonOrder FOREIGN KEY (PersonID)
-    REFERENCES Persons(PersonID)
-
+	codPersona INT NOT NULL,
+	codEmpresa INT NOT NULL,
 );
 
+CREATE TABLE Administradores(
+	codPersona INT NOT NULL,
+	codAdmin INT NOT NULL
+);
+
+
+--Cosas que ya de por si suenan dependientes
+CREATE TABLE Productos(
+	id INT IDENTITY(1, 1),
+	codProducto VARCHAR(45) NOT NULL PRIMARY KEY,
+	nombreProducto VARCHAR(50) NOT NULL,
+	descProducto VARCHAR(50) NOT NULL,
+	precioProducto FLOAT NOT NULL,
+	envioProducto FLOAT NOT NULL,
+
+	pathFoto VARCHAR(45) NOT NULL,
+	
+	CreatedAt TIMESTAMP NOT NULL,
+	LastUpdate TIMESTAMP NOT NULL
+);
+
+CREATE TABLE Categorias(
+	id INT IDENTITY(1, 1),
+	codTag VARCHAR(45) NOT NULL PRIMARY KEY,
+	nombreTag VARCHAR(20) NOT NULL,
+);
+
+CREATE TABLE ProductoCategorias(
+	codProducto VARCHAR(45) NOT NULL,
+	codTag VARCHAR(45) NOT NULL
+);
+
+CREATE TABLE Comentarios(
+	id INT IDENTITY(1, 1),
+	codComentario VARCHAR(45) NOT NULL,
+	codProducto VARCHAR(45) NOT NULL,
+	codPersona VARCHAR(45) NOT NULL,
+	contenidoComentario VARCHAR(120) NOT NULL,
+	
+	CreatedAt TIMESTAMP NOT NULL,
+	LastUpdate TIMESTAMP NOT NULL
+);
+
+CREATE TABLE Ordenes(
+	id INT IDENTITY(1, 1) NOT NULL,
+	codOrden VARCHAR(45) NOT NULL PRIMARY KEY,
+	codEmpresa VARCHAR(45) NOT NULL,
+	codPersona VARCHAR(45) NOT NULL,
+	fechaOrden DATETIME NOT NULL,
+	fechaEntregaOrden DATETIME NOT NULL,
+	fechaPagoOrden DATETIME NOT NULL,
+);
+
+CREATE TABLE OrdenProductos(
+	id INT IDENTITY(1, 1),
+	codOrdenProducto VARCHAR(45) NOT NULL PRIMARY KEY,
+	codOrden
+	codProducto VARCHAR(45),
+	cantidadProducto INT NOT NULL,
+	precioTotal FLOAT NOT NULL,
+	
+);
