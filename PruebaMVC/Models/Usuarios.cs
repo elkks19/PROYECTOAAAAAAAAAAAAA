53b373fe0d168a28db417489347f7616135f9818
Rafael <rafafabiani1909@gmail.com>
2023-09-19 18:31:07 -0400

    En PruebaMVC pues ando haciendo eso xd, probar el MVC

---
+++ PruebaMVC/Models/Usuarios.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PruebaMVC.Models
+{
+    public class Usuarios
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string? Apellidos { get; set; }
+        public string Email { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime FechaNacimiento { get; set; }
+
+    }
+}
