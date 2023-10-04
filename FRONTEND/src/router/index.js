a7b23802f6d0e59dfe45387937a070e788fd8de9
Rafael <rafaelosinaga2903@gmail.com>
2023-10-04 01:05:58 -0400

    version fabio

--- FRONTEND/src/router/index.js
+++ FRONTEND/src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../Vistas/Home.vue'
+import RegistroEmpresa from '../Vistas/RegistroEmpresa.vue'
+import RegistroUsuario from '../Vistas/RegistroUsuario.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +10,16 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/RegistroEmpresa',
+      name: 'RegistroEmpresa',
+      component: RegistroEmpresa
+    },
+    {
+      path: '/RegistroUsuario',
+      name: 'RegistroUsuario',
+      component: RegistroUsuario
     },
   ]
 })
