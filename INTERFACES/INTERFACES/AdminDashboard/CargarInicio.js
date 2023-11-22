aa37d7bb002a61ee1c6cb4bb68c981f8d24d4f4a
Rafael <rafafabiani1909@gmail.com>
2023-11-21 23:22:35 -0400

    a

---
+++ INTERFACES/INTERFACES/AdminDashboard/CargarInicio.js
@@ -0,0 +1,25 @@
+const numeroElements = document.querySelectorAll('.numero');
+
+window.onload = function(){
+    const numUsuarios = document.getElementById("numUsuarios");
+    const numEmpresas = document.getElementById("numEmpresas");
+    const ventasTotales = document.getElementById("ventasTotales");
+
+    axios.get("http://localhost:5132/personas/UltimoMes/", {
+        Headers: {
+            'Content-Type': 'application/json',
+            'Authorization': localStorage.getItem("token"),
+            'codPersona': 'PER-004'
+        }
+    }).then(response =>{
+        console.log(response.data);
+        const cantEmpresas = response.data.empresas.reduce(sum => sum + 1, 0);
+        const cantUsuarios = response.data.usuarios.reduce(sum => sum + 1, 0);
+        const ventasTotales = response.data.ventasTotales;
+        numUsuarios.innerText = cantUsuarios;
+        numEmpresas.innerText = cantEmpresas;
+        ventasTotales.innerHTML = ventasTotales;
+    }).catch(error => {
+        console.log(error);
+    });
+}
